fa0334ece213284aea802cd7922c7561b91ac738
Olawale Shopeyin <shopeyinwale@gmail.com>
2025-11-20 18:21:52 +0000

    cors for prod

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/await-thenable */
 /* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
